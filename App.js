4356ff139327bc306173ee6b4acf8dd6c0a253a9
unknown <scriptewepro4@gmail.com>
2022-05-01 21:22:18 +0300

    FoodApp

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import EventForm from './EventForm';
+import EventList from './EventList';
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="EventList">
+        <Stack.Screen name="EventList" component={EventList} options={{ title: '                                                                                                                                                         HomePage       Menu       Events     Blog      Contact'}} />
+        <Stack.Screen name="EventForm" component={EventForm} options={{ title: 'Products' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
